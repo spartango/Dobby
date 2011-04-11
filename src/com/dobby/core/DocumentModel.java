164c4a209efb3c5d76459fda80ecab9e7d14e250
John Capodilupo <johncapodilupo@dhcp-0104804661-27-fb.client.student.harvard.edu>
2011-04-10 21:19:35 -0400

    Added Node class to DocumentModel

--- src/com/dobby/core/DocumentModel.java
+++ src/com/dobby/core/DocumentModel.java
@@ -1,5 +1,30 @@
 package com.dobby.core;
 
+class Node<N,E> {
+	private N vertex;
+	private E outGoingEdges;
+	
+	public Node(N vertex, E outGoingEdges) {
+		this.vertex = vertex;
+		this.outGoingEdges = outGoingEdges;
+	}
+	public N getVertex() {
+		return vertex;
+	}
+	public void setVertex(N vertex) {
+		this.vertex = vertex;
+	}
+	public E getOutGoingEdges() {
+		return outGoingEdges;
+	}
+	public void setOutGoingEdges(E outGoingEdges) {
+		this.outGoingEdges = outGoingEdges;
+	}
+	
+} 
+
+//TODO Add Edge class.  Then declare documentmodel to be a directed graph using the node and edges as above
 public class DocumentModel {
 
+	
 }
