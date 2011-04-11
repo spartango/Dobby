e9db962a36c54f9cf8306aff9d0dc1979b883ae3
John Capodilupo <johncapodilupo@dhcp-0104804661-27-fb.client.student.harvard.edu>
2011-04-10 23:12:42 -0400

    Added edges and document model.

--- src/com/dobby/core/DocumentModel.java
+++ src/com/dobby/core/DocumentModel.java
@@ -1,5 +1,9 @@
 package com.dobby.core;
 
+import java.util.Vector;
+
+import org.jgrapht.DirectedGraph;
+
 class Node<N,E> {
 	private N vertex;
 	private E outGoingEdges;
@@ -23,8 +27,51 @@ class Node<N,E> {
 	
 } 
 
-//TODO Add Edge class.  Then declare documentmodel to be a directed graph using the node and edges as above
-public class DocumentModel {
+class Edge<N,E>{
+	private N source;
+	private N target;
+	private E request;
+	
+	public Edge(N source, N target, E request) {
+		this.source = source;
+		this.target = target;
+		this.request = request;
+	}
+	public N getSource() {
+		return source;
+	}
+	public void setSource(N source) {
+		this.source = source;
+	}
+	public N getTarget() {
+		return target;
+	}
+	public void setTarget(N target) {
+		this.target = target;
+	}
+	public E getRequest() {
+		return request;
+	}
+	public void setRequest(E request) {
+		this.request = request;
+	}
+}
 
+public class DocumentModel {
+	private DirectedGraph<Node<Vector<Integer>, Request>, Edge<Vector<Integer>, Request>> interactionModel;
+
+	public DocumentModel(DirectedGraph<Node<Vector<Integer>,Request>,Edge<Vector<Integer>,Request>> interactionModel) {
+		super();
+		this.interactionModel = interactionModel;
+	}
+
+	public DirectedGraph<Node<Vector<Integer>, Request>, Edge<Vector<Integer>, Request>> getInteractionModel() {
+		return interactionModel;
+	}
+
+	public void setInteractionModel(
+			DirectedGraph<Node<Vector<Integer>, Request>, Edge<Vector<Integer>, Request>> interactionModel) {
+		this.interactionModel = interactionModel;
+	}
 	
 }
