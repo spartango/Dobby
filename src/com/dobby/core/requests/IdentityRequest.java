d9b983ff14b69474d71f00607b948589f6ce0f0e
John Capodilupo <johncapodilupo@dhcp-0059505947-63-71.client.student.harvard.edu>
2011-04-18 13:30:52 -0400

    Added IdentityRequest class

---
+++ src/com/dobby/core/requests/IdentityRequest.java
@@ -0,0 +1,37 @@
+package com.dobby.core.requests;
+
+import com.dobby.core.Request;
+import com.dobby.core.StateVector;
+
+public class IdentityRequest extends Request {
+
+	/**
+	 * Creates a new Identity Request with the supplied state
+	 * @param user
+	 * @param stateVector
+	 * @param serialNumber
+	 */
+	public IdentityRequest(String user, StateVector stateVector,
+			int serialNumber) {
+		super(user, stateVector, serialNumber, 0, 'a');
+	}
+	
+	/**
+	 * Transforms this request with respect to another request based 
+	 * on transformation rules:
+	 * @param r 
+	 */
+	@Override
+	public Request transform(Request r) {
+		return this;
+	}
+
+	/**
+	 * Applies the identity operation to the target string
+	 * @param target
+	 */
+	public String apply(String target) {
+		return target;
+	}
+
+}
