861490f2d361b778f860f7bdf95d994360141dd6
Anand Gupta <anand@dhcp-0104820480-f2-93.client.student.harvard.edu>
2011-04-19 04:08:46 -0400

    statevectors tested

--- src/com/dobby/tests/core/StateVectorTest.java
+++ src/com/dobby/tests/core/StateVectorTest.java
@@ -1,16 +1,34 @@
 package com.dobby.tests.core;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import com.dobby.core.StateVector;
 
 public class StateVectorTest {
 
 	@Test
 	public void testIncrement() {
-
+		StateVector testVector = new StateVector();
+		testVector.put("Test", 0);
+		testVector.incrementUser("Test");
+		testVector.incrementUser("Test");
+		testVector.incrementUser("Testx");
+		assertTrue(testVector.getUser("Test") == 2
+				&& testVector.getUser("Testx") == 1);
 	}
 
 	@Test
 	public void testDecrement() {
-
+		StateVector testVector = new StateVector();
+		testVector.put("Test", 2);
+		testVector.decrementUser("Test");
+		assertTrue(testVector.getUser("Test") == 1
+				&& testVector.getUser("Testx") == 0);
+		testVector.decrementUser("Test");
+		testVector.decrementUser("Testx");
+		assertTrue(testVector.getUser("Test") == 0
+				&& testVector.getUser("Testx") == 0);
 	}
 }
--- src/com/dobby/tests/request/DeleteRequestTest.java
+++ src/com/dobby/tests/request/DeleteRequestTest.java
@@ -7,54 +7,49 @@ import org.junit.Test;
 import com.dobby.core.Request;
 import com.dobby.core.StateVector;
 import com.dobby.core.requests.DeleteRequest;
+import com.dobby.core.requests.InsertRequest;
 
 public class DeleteRequestTest {
 
 	@Test
 	public void testInsertTransform() {
 		String testString = "0123456789abcdef";
-		Request d1 = new DeleteRequest("Test", new StateVector(), 0, 
-				2, '2');
-		Request d2 = new DeleteRequest ("Exam", new StateVector(), 1,
-				7, '7');
+		Request d1 = new DeleteRequest("Test", new StateVector(), 0, 14, 'e');
+		Request d2 = new InsertRequest("Exam", new StateVector(), 1, 7, 'y');
+		Request transform = d1.transform(d2);
+		String product = transform.apply(d2.apply(testString));
+		System.out.println(testString + " Delete-Ins -> " + product);
+		assertTrue(product.equals("0123456y789abcdf"));
+
 	}
 
 	@Test
 	public void testDeleteTransform() {
-		DeleteRequest delete = new DeleteRequest("Test", new StateVector(), 0, 
-				11, 'b');
+
 		String testString = "0123456789abcdef";
-		Request d1 = new DeleteRequest("Test", new StateVector(), 0, 
-				2, '2');
-		Request d2 = new DeleteRequest ("Exam", new StateVector(), 1,
-				7, '7');
+		Request d1 = new DeleteRequest("Test", new StateVector(), 0, 2, '2');
+		Request d2 = new DeleteRequest("Exam", new StateVector(), 1, 7, '7');
 		Request transform = d2.transform(d1);
 		String product = transform.apply(d1.apply(testString));
-		System.out.println(testString + " Double delete produced, First" +
-				" request positioned before second " + product);
+		System.out.println(testString + " Double delete produced, First"
+				+ " request positioned before second " + product);
 		assertTrue(product.equals("01345689abcdef"));
-		
+
 		String testString2 = "0123456789abcdef";
-		Request d3 = new DeleteRequest("Test", new StateVector(), 0, 
-				2, '2');
-		Request d4 = new DeleteRequest ("Exam", new StateVector(), 1,
-				1, '1');
+		Request d3 = new DeleteRequest("Test", new StateVector(), 0, 2, '2');
+		Request d4 = new DeleteRequest("Exam", new StateVector(), 1, 1, '1');
 		Request transform2 = d4.transform(d3);
 		String product2 = transform2.apply(d3.apply(testString2));
-		System.out.println(testString2 + " Double delete produced, " +
-				"request positioned after second" + product2);
+		System.out.println(testString2 + " Double delete produced, "
+				+ "request positioned after second" + product2);
 		assertTrue(product2.equals("03456789abcdef"));
-		
-		
+
 		String testString3 = "0123456789abcdef";
-		Request d5 = new DeleteRequest("Test", new StateVector(), 0, 
-				2, '2');
-		Request d6 = new DeleteRequest ("Exam", new StateVector(), 1,
-				7, '7');
+		Request d5 = new DeleteRequest("Test", new StateVector(), 0, 2, '2');
+		Request d6 = new DeleteRequest("Exam", new StateVector(), 1, 7, '7');
 		Request transform3 = d6.transform(d5);
 		String product3 = transform3.apply(d5.apply(testString3));
-		System.out.println(testString3 + "Double delete produced" +
-				product3);
+		System.out.println(testString3 + "Double delete produced" + product3);
 		assertTrue(product3.equals("01345689abcdef"));
 	}
 
